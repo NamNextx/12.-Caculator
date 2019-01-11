0adea852f508d2e410b5e195eebd062b5a9abad7
Namnextx <phuongnam.nextx@gmail.com>
2019-01-11 12:18:05 +0700

    2. Da hoan thanh ver1

---
+++ funcfile.js
@@ -0,0 +1,35 @@
+
+function cong() {
+    datain=document.getElementById("fnumber").value;
+    dataout=document.getElementById("snumber").value;
+   // document.write(datain+dataout);
+    var int=parseInt(dataout)+parseInt(datain);
+    var cong=document.getElementById('idresult');
+    cong.innerHTML=int;
+}
+function tru() {
+    datain=parseInt(document.getElementById("fnumber").value);
+    dataout=parseInt(document.getElementById("snumber").value);
+    // document.write(datain+dataout);
+    var int=parseInt(datain- dataout);
+    var tru=document.getElementById('idresult');
+    tru.innerHTML=int;
+}
+function nhan() {
+    datain=document.getElementById("fnumber").value;
+    dataout=document.getElementById("snumber").value;
+    // document.write(datain+dataout);
+    var int=parseInt(dataout)*parseInt(datain);
+    var nhan1=document.getElementById('idresult');
+    nhan1.innerHTML=int;
+}
+function chia() {
+    datain=document.getElementById("fnumber").value;
+    dataout=document.getElementById("snumber").value;
+    // document.write(datain+dataout);
+    var int=parseInt(datain) / parseInt(dataout);
+    var chia=document.getElementById('idresult');
+    chia.innerHTML=int;
+}
+function tinhToan() {
+}
